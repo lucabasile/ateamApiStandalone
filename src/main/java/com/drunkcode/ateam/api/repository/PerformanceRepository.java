ce587ac0e2ebef61c05b35b2152bf01eeaaef200
unknown <luca.basile.76@gmmail.com>
2019-02-24 17:35:05 +0100

    embedded ID 3

--- src/main/java/com/drunkcode/ateam/api/repository/PerformanceRepository.java
+++ src/main/java/com/drunkcode/ateam/api/repository/PerformanceRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import com.drunkcode.ateam.api.model.Performance;
+import com.drunkcode.ateam.api.model.PerformanceID;
 @Component
-public interface PerformanceRepository extends JpaRepository<Performance, Long>,PerformanceRepositoryCustom{
+public interface PerformanceRepository extends JpaRepository<Performance, PerformanceID>,PerformanceRepositoryCustom{
 	
 	
 
