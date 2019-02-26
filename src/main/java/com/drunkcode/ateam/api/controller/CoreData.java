939fd30700d10d45c9ab6b8590c96b745c0fa358
Basile Luca <BasileLu@valueteam.com>
2019-02-26 12:27:27 +0100

    fuffa

--- src/main/java/com/drunkcode/ateam/api/controller/CoreData.java
+++ src/main/java/com/drunkcode/ateam/api/controller/CoreData.java
@@ -1,7 +1,6 @@
 package com.drunkcode.ateam.api.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
