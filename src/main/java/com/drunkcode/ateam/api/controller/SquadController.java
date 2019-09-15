b8ef083eb298210138a7ae9838770c3274e81510
Basile Luca <BasileLu@valueteam.com>
2019-09-16 00:19:41 +0200

    squadController

--- src/main/java/com/drunkcode/ateam/api/controller/SquadController.java
+++ src/main/java/com/drunkcode/ateam/api/controller/SquadController.java
@@ -1,16 +1,32 @@
 package com.drunkcode.ateam.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.drunkcode.ateam.api.dto.Squad;
 
 @RestController
 @RequestMapping("/squad")
 public class SquadController {
 
 	
-	@GetMapping("/{season}/{day}/{teamId}")
-	public void getSquad() {
+	@GetMapping("/{leagueId}/{seasonId}/{day}/{teamId}")
+	public void getSquad(@PathVariable long seasonId,@PathVariable Integer day,@PathVariable long teamId) {
+		
+	}
+	
+	@PostMapping("/{season}/{day}/{teamId}")
+	public void createSquad(@PathVariable long seasonId,@PathVariable Integer day,@PathVariable long teamId,@RequestBody Squad squad) {
+		
+	}
+	
+	@PutMapping("/{season}/{day}/{teamId}")
+	public void updateSquad(@PathVariable long seasonId,@PathVariable Integer day,@PathVariable long teamId,@RequestBody Squad squad) {
 		
 	}
 }
